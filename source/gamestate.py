dde012fa7b71080a923d31b710a02b988b851feb
Reece Heinlein <reeceheinlein@gmail.com>
2011-02-13 21:31:56 -0500

    State object stub. Experimenting with object scaling
    in the main loop.

---
+++ source/gamestate.py
@@ -0,0 +1,12 @@
+# GameState
+#   Stores the stage of a game.
+# Author - Reece Heinlein
+
+class GameState:
+    """Class to contain the state of a game"""
+    def __init__(self):
+        self.players = None
+        self.board = None
+        self.objective_deck = None
+        self.point_limit = 0
+        self.strategies = None
--- source/main.py
+++ source/main.py
@@ -20,6 +20,12 @@ def main():
     screen = pygame.display.set_mode((640, 480))
     pygame.display.set_caption('Twilight Imperium')
 
+    img = pygame.image.load('data/images/tiles/sardakk_norr.png')
+    img = img.convert_alpha()
+    img_rect = img.get_rect()
+    img = pygame.transform.scale(img, (img_rect.width/3, img_rect.height/3))
+    img_rect = img.get_rect()
+
     background = pygame.Surface(screen.get_size())
     background = background.convert()
     background.fill((250, 250, 250))
@@ -28,8 +34,10 @@ def main():
     text = font.render("Welcome to Twilight Imperium", 1, (10, 10, 10))
     textpos = text.get_rect()
     textpos.centerx = background.get_rect().centerx
-    textpos.centery = background.get_rect().centery
+    img_rect.centerx = background.get_rect().centerx
+    img_rect.centery = background.get_rect().centery + 50
     background.blit(text, textpos)
+    background.blit(img, img_rect)
 
     screen.blit(background, (0, 0))
     pygame.display.flip()
