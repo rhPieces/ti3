d2b9bd55938d0c91cd8c6350b1d3409e9977a0cd
Reece Heinlein <reeceheinlein@gmail.com>
2011-02-22 00:35:01 -0500

    create_helper changes for adding hexes

--- source/util/create_helper.py
+++ source/util/create_helper.py
@@ -1,4 +1,5 @@
 import os
+from pprint import pprint
 from pymongo.connection import Connection
 from pymongo.son_manipulator import AutoReference, NamespaceInjector
 
@@ -27,7 +28,7 @@ specialties = {
     'g': 'Green',
     'b': 'Blue',
     'y': 'Yellow',
-    'g': 'Ground Force',
+    'gf': 'Ground Force',
     's': 'Shock Troop',
     'f': 'Fighter',
     't': 'Trade Good',
@@ -48,23 +49,35 @@ def add_hex():
     new_hex['set'] = get_from_dict('Set', sets)
     new_hex['type'] = get_from_dict('Type', types)
     new_hex['entities'] = get_entities()
-    #db.hexes.save(new_hex)
-    print(new_hex)
+
+    pprint(new_hex)
+    if raw_input('Save? y/N ') == 'y':
+        db.hexes.save(new_hex)
+    else:
+        for ent in new_hex['entities']:
+            if '_id' in ent:
+                db.planets.remove(ent)
 
 def add_planet():
     planet = {}
     planet['name'] = raw_input('Planet Name: ')
-    planet['res'] = raw_input('Resource Value: ')
-    planet['inf'] = raw_input('Influence Value: ')
+    try:
+        planet['res'] = int(raw_input('Resource Value: '))
+        planet['inf'] = int(raw_input('Influence Value: '))
+    except ValueError, e:
+        print('Caught a bad number for resource or influence')
+        planet['res'] = planet['inf'] = 0
     planet['specialty'] = get_from_dict('Specialty', specialties)
 
-    #db.planets.save(planet)
+    pprint(planet)
+    if raw_input('Save? y/N ') == 'y':
+        db.planets.save(planet)
     return planet
 
 def get_entities():
     to_add = []
-    while raw_input('Add Entity? y/N ') == 'y':
-        if (raw_input('Planet? y/N ') == 'y'):
+    while raw_input('Add Entity? Y/n ') != 'n':
+        if raw_input('Planet? Y/n ') != 'n':
             to_add.append(add_planet())
         else:
             to_add.append(get_from_dict('Entity', entities))
@@ -84,6 +97,6 @@ def get_from_dict(name, ops):
     return None
 
 if __name__ == '__main__':
-    while raw_input('Continue? ') == 'y':
+    while raw_input('Continue? Y/n ') != 'n':
         os.system(['clear','cls'][os.name == 'nt'])
         add_hex()
